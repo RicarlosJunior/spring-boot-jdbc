4ac10afa58971ff845a9abd3bbb30ae3f5fd5dbe
Ricarlos Junior <ricarlosjr@hotmail.com>
2024-09-25 20:29:54 -0300

    Trabalhando com a branch de desenvolvimento para refatorar codigo.

--- projeto-java-jdbc/src/main/java/br/com/jdbc/persistence/ConnectionUtil.java
+++ projeto-java-jdbc/src/main/java/br/com/jdbc/persistence/ConnectionUtil.java
@@ -6,7 +6,11 @@ import java.sql.SQLException;
 
 public class ConnectionUtil {
 	
+	private static final String url = "jdbc:mysql://localhost:3306/java-jdbc";
+	private static final String user = "root";
+	private static final String password = "root";
+	
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost:3306/java-jdbc", "root", "root");
+		return DriverManager.getConnection(url, user, password);
 	}
 }
